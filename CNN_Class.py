10addf0843ef8c3954862565599355e5f31ef015
ggarrett13 <g.h.garrett13@gmail.com>
2019-06-06 16:58:13 +0200

    Multi-gpu

--- CNN_Class.py
+++ CNN_Class.py
@@ -66,6 +66,13 @@ class CNN:
 
         if use_gpu:
             net = net.cuda()
+            if torch.cuda.device_count() > 1:
+                print("Let's use", torch.cuda.device_count(), "GPUs!")
+                # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+                net = nn.DataParallel(net)
+
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            net.to(device)
 
         # Loss function
         # with optim, can also use e.g. Adam
