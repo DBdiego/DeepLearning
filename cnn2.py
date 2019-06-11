2b34faf6cdc67e7468483ee7aa42f7d645342972
DBdiego <diego.de.buysscher@gmail.com>
2019-06-11 17:41:44 +0200

    reduced the dataset size

--- cnn2.py
+++ cnn2.py
@@ -23,28 +23,25 @@ class Net(nn.Module):
         self.dim1 = dim1
         print(n_c_layers, dim1, kernel_conv, stride_conv, kernel_pool, stride_pool, n_l_layers, dim2)
 
-        final_dim = 50 # decreases in size with each convolution and pooling layer
+        final_dim = 224 # decreases in size with each convolution and pooling layer
         super(Net, self).__init__()
 
         # Defining convolution layers
         self.n_c_layers = n_c_layers
         for i in range(n_c_layers):
+            print(i)
             pad = int(kernel_conv[i]/2) # padding such that convolution shits out roughly same size
             setattr(self, f"conv{i}", nn.Conv2d(in_channels=self.dim1[i], out_channels=self.dim1[i+1],
                                                 kernel_size=kernel_conv[i], stride=stride_conv[i], padding=pad))
-            
             #(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-            self.pool = nn.MaxPool2d(kernel_pool[i], stride_pool[i]) #(kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+            self.pool = nn.MaxPool2d(kernel_pool[i], stride_pool[i]) # (kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
 
             # Updating changing of image size
             final_dim = int((final_dim - kernel_conv[i] + 2 * pad) / stride_conv[i] + 1)
-            final_dim = int((final_dim - kernel_pool[i] + 2 * 0  ) / stride_pool[i] + 1)
-            
+            final_dim = int((final_dim - kernel_pool[i] + 2 * 0) / stride_pool[i] + 1)
         self.final_dim = final_dim
-        
         # adding first layer (image to nodes):
         dim2.insert(0, self.dim1[-1]*self.final_dim*self.final_dim)
-        
         # Defining network linear layers
         self.n_l_layers = n_l_layers
         for i in range(n_l_layers):
