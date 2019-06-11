3e4a3abb7107cb05a95f785c03bf91ee4cf1a129
DBdiego <diego.de.buysscher@gmail.com>
2019-06-11 16:43:24 +0200

    remove useless prints

--- cnn2.py
+++ cnn2.py
@@ -4,7 +4,7 @@ import re
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
+
 
 '''
 n_c_layers:   int, number of convolution layers
@@ -23,24 +23,28 @@ class Net(nn.Module):
         self.dim1 = dim1
         print(n_c_layers, dim1, kernel_conv, stride_conv, kernel_pool, stride_pool, n_l_layers, dim2)
 
-        final_dim = 224 # decreases in size with each convolution and pooling layer
+        final_dim = 50 # decreases in size with each convolution and pooling layer
         super(Net, self).__init__()
 
         # Defining convolution layers
         self.n_c_layers = n_c_layers
         for i in range(n_c_layers):
-            print(i)
             pad = int(kernel_conv[i]/2) # padding such that convolution shits out roughly same size
             setattr(self, f"conv{i}", nn.Conv2d(in_channels=self.dim1[i], out_channels=self.dim1[i+1],
                                                 kernel_size=kernel_conv[i], stride=stride_conv[i], padding=pad))
+            
             #(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-            self.pool = nn.MaxPool2d(kernel_pool[i], stride_pool[i]) # (kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+            self.pool = nn.MaxPool2d(kernel_pool[i], stride_pool[i]) #(kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+
             # Updating changing of image size
             final_dim = int((final_dim - kernel_conv[i] + 2 * pad) / stride_conv[i] + 1)
-            final_dim = int((final_dim - kernel_pool[i] + 2 * 0) / stride_pool[i] + 1)
+            final_dim = int((final_dim - kernel_pool[i] + 2 * 0  ) / stride_pool[i] + 1)
+            
         self.final_dim = final_dim
+        
         # adding first layer (image to nodes):
         dim2.insert(0, self.dim1[-1]*self.final_dim*self.final_dim)
+        
         # Defining network linear layers
         self.n_l_layers = n_l_layers
         for i in range(n_l_layers):
@@ -61,5 +65,3 @@ class Net(nn.Module):
             else:
                 x = getattr(self, f"fc{i}")(x)
         return x
-
-
