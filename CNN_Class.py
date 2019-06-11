b8d982410a02ae32ee4d8b8a2448fc34dbb1a4bd
Stavrow <stavrowbahnam@gmail.com>
2019-06-11 14:16:27 +0200

    hopefully multiple gpu in device

--- CNN_Class.py
+++ CNN_Class.py
@@ -66,12 +66,13 @@ class CNN:
 
         if use_gpu:
             net = net.cuda()
+            print(torch.cuda.device_count())
             if torch.cuda.device_count() > 1:
                 print("Let's use", torch.cuda.device_count(), "GPUs!")
                 # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
                 net = nn.DataParallel(net)
 
-            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
             net.to(device)
 
         # Loss function
