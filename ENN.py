19f7e783d1dc6f35e40bacd895de7557d122a3d6
DBdiego <diego.de.buysscher@gmail.com>
2019-06-12 15:45:41 +0200

    got the results saved

--- ENN.py
+++ ENN.py
@@ -29,8 +29,8 @@ def argument_input_interface(
         kernel_pool,
         stride_pool,
         n_layers,
-        dim1=32,
-        dim2=120
+        dim1=400,
+        dim2=200
 ):
     _dim1 = np.linspace(3, dim1, int(n_conv + 1)).astype(int)
     _dim1 = np.delete(_dim1, 0)
@@ -119,7 +119,7 @@ if __name__ == "__main__":
 
     start_time_archi = time.time()
     print('Creating Archipelago: ...')
-    archi = pg.archipelago(n=8,algo=algo, prob=problem, pop_size=1)
+    archi = pg.archipelago(n=16,algo=algo, prob=problem, pop_size=1)
     print(f'Creating Archipelago: DONE ({round(time.time() - start_time_archi, 5)}s)\n')
     
     print('\n===============================================')
