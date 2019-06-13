b4cf3eec9315256039f8475b68a186b4dbf9e0f4
Nikhil Wessendorp <n.wessendorp@gmail.com>
2019-06-13 15:06:38 +0200

    file to test multiple gpus

---
+++ generation_creation.py
@@ -0,0 +1,33 @@
+import numpy as np
+from CNN_Class import CNN, random_split, CustomDataset
+import torch
+import time
+
+
+NORMALIZE = True
+IMAGE_PATH = 'database/'
+RATIO_TRAINING = 0.1
+RATIO_DATA = 0.1
+MAX_DATA = RATIO_DATA*41556
+
+print('Running gen_creat.py\n')
+
+print('Importing data: ...')
+dataset = CustomDataset(image_path=IMAGE_PATH, normalise=NORMALIZE, maxx=MAX_DATA, train=True)
+print('Importing data: DONE\n')
+
+I = int(RATIO_TRAINING * len(dataset))
+lengths = [len(dataset) - I, I]  # train data and test data
+train_dataset, test_dataset = random_split(dataset, lengths)
+
+if torch.cuda.is_available():
+    num_avail_gpus = torch.cuda.device_count()
+else:
+    num_avail_gpus = 1
+genome1 = [5, [82, 161, 241, 320, 400], [2, 2, 2, 2, 2], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], 7, [200, 173, 146, 120, 93, 66, 40]]
+genome2 = [6, [69, 135, 201, 267, 333, 400], [2, 2, 2, 2, 2, 2], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2, 2], 7, [200, 173, 146, 120, 93, 66, 40]]
+genomes = [genome1,genome2]
+
+for i in range(num_avail_gpus):
+    print(i)
+    CNN(i, train_dataset, test_dataset, genomes[i][0], genomes[i][1],genomes[i][2],genomes[i][3],genomes[i][4],genomes[i][5],genomes[i][6],genomes[i][7])
