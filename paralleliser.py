053c1f7c2d356cd6b3e39b7a5ddbf0c719c832c7
Nikhil Wessendorp <n.wessendorp@gmail.com>
2019-06-14 19:12:40 +0200

    yes

--- paralleliser.py
+++ paralleliser.py
@@ -94,7 +94,7 @@ def fitness_func(genomes,train_dataset, test_dataset, results_HL):
 
         print('Cycle done\n')
 
-    j = 1
+    j = 0
     results_final = results.copy()
     for i in range(len(results_HL)):
         if results_HL[i] == None:
