388c87e04c3aa9345d2adbef643f1e563ed55c3d
DBdiego <diego.de.buysscher@gmail.com>
2019-06-14 22:06:20 +0200

    got the results saved

--- CNN_Class.py
+++ CNN_Class.py
@@ -27,8 +27,8 @@ class CNN:
         # --------------------------------------
         # Parameters:
         MAXTRAINTIME = 4*60  # seconds, not sure if this is a good time. Note that testing time is not included, this is (often) slightly less than 1 epoch time.
-        BATCH_SIZE = 10
-        LR = 0.001
+        BATCH_SIZE = 40
+        LR = 1E-3
         MOMENTUM = 0.9
         CONVERGENCE = 0.001  # Not sure if this is a good value (smaller change than 0.1%)
         MIN_EPOCH = 6  # should be 6 or higher, it can have less epochs in results if the MAXTRAINTIME is exceeded.
@@ -117,7 +117,7 @@ class CNN:
                 
                 every_x_minibatches = 200 # print every X mini-batches
                 if i % every_x_minibatches == every_x_minibatches-1:  
-                    print(f'\t {network_index} --> [{epoch}, {i + 1}] loss: {np.round(running_loss / every_x_minibatches, 4)}')
+                    print(f'\t N{network_index}:   [{epoch}, {i + 1}] loss: {np.round(running_loss / every_x_minibatches, 4)}')
                     #print(outputs, labels)
                     running_loss_epoch += running_loss
                     running_loss = 0.0
