2523f5a0e0f14a26a4eea519f8a80c45b8d0c453
DBdiego <diego.de.buysscher@gmail.com>
2019-06-17 01:31:33 +0200

    another commit

--- CNN_Class.py
+++ CNN_Class.py
@@ -155,6 +155,8 @@ class CNN:
             self.tot_epoch = epoch
             self.losslst = losslst
 
+            print(np.array(losslst))
+            print(np.diff(np.array(losslst)))
             print(np.average(np.diff(np.array(losslst))), 0.01*np.average(np.array(losslst)))
             if np.average(np.diff(np.array(losslst))) < 0.01*np.average(np.array(losslst)):
                 LR = LR/10
