ee4d0ba78d8ed85ebcb6d0fb7b20dce5100fd756
DBdiego <diego.de.buysscher@gmail.com>
2019-06-16 21:02:07 +0200

    another commit

--- CNN_Class.py
+++ CNN_Class.py
@@ -42,8 +42,8 @@ class CNN:
         # --------------------------------------
         # Parameters:
         MAXTRAINTIME = 15*60  # seconds, not sure if this is a good time. Note that testing time is not included, this is (often) slightly less than 1 epoch time.
-        BATCH_SIZE = 10
-        LR = 1*1E-6
+        BATCH_SIZE = 40
+        LR = 1*1E-5
         MOMENTUM = 0.5
         CONVERGENCE = 1E-5  # Not sure if this is a good value (smaller change than 0.001%)
         MIN_EPOCH = 10  # should be 6 or higher, it can have less epochs in results if the MAXTRAINTIME is exceeded.
